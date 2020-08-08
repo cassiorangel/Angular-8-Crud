d4cbc4fd8465573ab31a400de303fc3ca3425aff
Cassio Rangel <cassio.rfernandes@gmail.com>
2020-08-07 21:39:25 -0300

    Testando mascara

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
+
+const maskConfig: Partial<IConfig> = {
+  validation: false,
+};
 
 
 @NgModule({
@@ -11,6 +16,7 @@ import { LoginComponent } from './login/login.component';
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    NgxMaskModule.forRoot(maskConfig),
     AuthRoutingModule
   ]
 })
